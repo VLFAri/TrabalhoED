3c51aa518b42d475e79005b903347c1ae3f76897
Ari V.L. Filho <147186503+VLFAri@users.noreply.github.com>
2024-11-02 16:56:57 -0400

    Add files via upload

---
+++ Trabalho/src/Trabalho.java
@@ -0,0 +1,17 @@
+package trabalhoed;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Trabalho {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Inserir documentos: ");
+		String documento = sc.next();
+		InserirDocumento id = new InserirDocumento();
+		id.inserirArtigos(documento);
+		
+		
+	
+	}
+}
