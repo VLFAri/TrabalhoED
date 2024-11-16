486fcbdbbdf29fc29e2bfeb6de0ead039293d678
Ari V.L. Filho <147186503+VLFAri@users.noreply.github.com>
2024-11-15 22:13:02 -0400

    Update HashEntry.java

--- src/hash/HashEntry.java
+++ src/hash/HashEntry.java
@@ -7,25 +7,44 @@ package hash;
  * @param <K> o tipo da chave
  * @param <V> o tipo do valor
  */
-public class HashEntry<V> {
-    private V valor;
+public class HashEntry {
+	private String nome;
+    private String valor;
     
     /**
      * Construtor para criar uma nova entrada de hash com um valor.
      *
      * @param valor o valor a ser associado
      */
-    public HashEntry(V valor) {
+    public HashEntry(String nome, String valor) {
+    	this.nome = nome;
         this.valor = valor;
     }
     
+    /**
+     * Retorna o nome associado a esta entrada.
+     *
+     * @return o valor
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Define o nome para esta entrada.
+     *
+     * @param nome o nome a ser definido
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     /**
      * Retorna o valor associado a esta entrada.
      *
      * @return o valor
      */
-    public V getValor() {
+    public String getValor() {
         return valor;
     }
 
@@ -34,7 +53,7 @@ public class HashEntry<V> {
      *
      * @param valor o valor a ser definido
      */
-    public void setValor(V valor) {
+    public void setValor(String valor) {
         this.valor = valor;
     }
 }
