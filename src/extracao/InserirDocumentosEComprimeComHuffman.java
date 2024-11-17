b5c95705bcefd430f0c50e4b6cdd1daaf167ddb9
Ari V.L. Filho <147186503+VLFAri@users.noreply.github.com>
2024-11-17 17:20:42 -0400

    Update InserirDocumentosEComprimeComHuffman.java

--- src/extracao/InserirDocumentosEComprimeComHuffman.java
+++ src/extracao/InserirDocumentosEComprimeComHuffman.java
@@ -1,23 +1,30 @@
 package extracao;
+import compactacao.ArvoreHuffman;
 import java.io.*;
 import java.util.*;
+
 /**
  * Nessa classe, iremos inserir os artigos da pasta na qual o caminho digitado pelo usuário aponta e extrair o conteúdo de cada arquivo.
  * Após a extração do texto, o mesmo será comprimido em binário através do código de Huffman (classe ArvoreHuffman)
- * O método principal (que foi chamado pela main) retornará uma tabela com um inteiro como chave (número do artigo na qual o texto foi comprimido) e uma String que representa o conteudo comprimido de cada arquivo.
+ *Essa classe retorna para main 3 mapas:
+  - TODOS OS MAPAS TEM COMO CHAVE UMA STRING QUE É O NOME DO ARQUIVO.TXT
+  - O primeiro mapa que retorna na Main tem como valor de cada chave o conteúdo do arquivo comprimido (utiliza-se do HUffman) para inserir na Hash.
+  - O segundo tem como valor de cada chave um array de Strings que contém cada palavra do artigo representado pela chave e será usado para inserir e buscar na Trie
+  - Já o terceiro é similar ao primeiro, porém o conteúdo do arquivo não é comprimido, e será usado para impressão do conteúdo de um determinado arquivo 
  */
+
 
 public class InserirDocumentosEComprimeComHuffman { 
 	
-	//Esse método fará com que a String digitada pelo usuário se torne um caminho para diretório
+	//Esse método fará com que a String digitada pelo usuário se torne um caminho para diretório (mesmo que não seja válido (será tratado depois))
 	public File inserirPasta(String docs) {
 		File diretorio = new File(docs);
 		return diretorio;
 	}
 	
-	//Método irá extrair o conteúdo de cada arquivo da pasta	
+	//Método irá extrair o conteúdo de cada arquivo da pasta.	
 	public String extrairConteudoParaComprimirouNao(File arquivo) throws FileNotFoundException{
-		Scanner escrevendoArquivo = new Scanner(arquivo);
+		Scanner escrevendoArquivo = new Scanner(arquivo); //Objeto para instanciar a interface que pega o conteúdo dos arquivos;
 
 		String texto = ""; //String para guardar o texto
 		while (escrevendoArquivo.hasNext()){ //Enquanto o arquivo tiver linha, vai rodando...
@@ -25,9 +32,10 @@ public class InserirDocumentosEComprimeComHuffman {
 			texto += conteudo + " "; //Concatena com o que já foi armazenado do arquivo
 		}
 		escrevendoArquivo.close(); //Quando terminar, fecha o arquivo
-		return texto.replaceAll("\\s$", ""); //Retorna o texto extraído
+		return texto.replaceAll("\\s$", ""); //Retorna o texto extraído sem o ultimo caracter, que é um espaço em branco (que ficou após a concatenação.
 	}
 	
+	//Esse ArrayList será inserido no mapa que será usado para inserir e buscar na Trie
 	public List<String> extrairConteudoParaBuscar(File arquivo) throws FileNotFoundException{
 		List<String> conteudo_arquivos = new ArrayList<String>();
 		Scanner escrevendoArquivo = new Scanner(arquivo);
@@ -62,17 +70,17 @@ public class InserirDocumentosEComprimeComHuffman {
 				if(listOfFiles[i].isFile()) { //Se for um arquivo txt
 					String texto = extrairConteudoParaComprimirouNao(listOfFiles[i]); //Essa variável irá chamar o método que ectrai o conteúdo do arquivo
 					ArvoreHuffman ah = new ArvoreHuffman(texto); //Método para instanciar a classe ArvoreHuffman (compressão de dados)
-					if(e_pComprimir) {
-						tabela_armazenaInformacao.put(listOfFiles[i].getName(),ah.Comprimir(texto)); //Irá armazenar na tabela o nOME do artigo/arquivo como chave e o texto comprimido (comprimido pela classe ArovreHuffman)
+					if(e_pComprimir) { //Caso seja true, é para comprimir
+						tabela_armazenaInformacao.put(listOfFiles[i].getName(),ah.Comprimir(texto)); //Irá armazenar na tabela o nome do artigo/arquivo como chave e o texto comprimido (comprimido pela classe ArovreHuffman)
 					}
-					else {
-						tabela_armazenaInformacao.put(listOfFiles[i].getName(),texto); //Irá armazenar na tabela o nOME do artigo/arquivo como chave e o texto comprimido (comprimido pela classe ArovreHuffman)
+					else { //Casp contrário, não comprime
+						tabela_armazenaInformacao.put(listOfFiles[i].getName(),texto); //Irá armazenar na tabela o nOME do artigo/arquivo como chave e o texto não comprimido
 					}
 				}
 			}
 			
 			if(!tabela_armazenaInformacao.isEmpty()) { //Se a tabela não está vazia, logo tem arquivos!
-				if(e_pComprimir) {
+				if(e_pComprimir) { //Se for true, imprime a mensagem
 					System.out.println("Documentos inseridos com sucesso!");
 				}
 				return tabela_armazenaInformacao; //Retornará a tabela com os valores associados
@@ -92,8 +100,9 @@ public class InserirDocumentosEComprimeComHuffman {
 		
 	}
 	
+	//Esse método retornará o mapa que será utilizado na Trie (inserindo palavra por palavra de cada artigo).
 	public Map<String,List<String>> textos_nComprimidos(String documento) throws FileNotFoundException{
-		//Tabela de map que irá guardar o nome do aritgo como chave e o conteudo comprimido em binário como a String
+		//Tabela de map que irá guardar o nome do aritgo como chave e um array para cada chave, com cada posição sendo ocupada por uma palavra do artigo representado pela chave
 		Map<String,List<String>> tabela_armazenaInformacao = new HashMap<>();
 				
 		//Array que será utilizado para armazenar arquivos
@@ -107,14 +116,13 @@ public class InserirDocumentosEComprimeComHuffman {
 			listOfFiles = dir.listFiles(); //A variável criada outrora irá armazenar cada arquivo do diteptorio dir em uma posição (se houver arquivos)
 			for(int i=0;i<listOfFiles.length;i++) { //Laço irá percorrer cada arquivo da pasta
 				if(listOfFiles[i].isFile()) { //Se for um arquivo txt
-					List<String> palavras_texto = extrairConteudoParaBuscar(listOfFiles[i]); //Essa variável irá chamar o método que ectrai o conteúdo do arquivo
-					tabela_armazenaInformacao.put(listOfFiles[i].getName(),palavras_texto); //Irá armazenar na tabela o nOME do artigo/arquivo como chave e o texto comprimido (comprimido pela classe ArovreHuffman)		
+					List<String> palavras_texto = extrairConteudoParaBuscar(listOfFiles[i]); //Essa variável irá chamar o método que devolve o array com as palavras do arquivo em cada posiçaõ
+					tabela_armazenaInformacao.put(listOfFiles[i].getName(),palavras_texto); //Irá armazenar na tabela o nOME do artigo/arquivo como chave e o Array com as palavras.		
 				}
 			}
-			return tabela_armazenaInformacao;
+			return tabela_armazenaInformacao; //Retorna o mapa
 		}
 		
-		return null;
+		return null; //Retorna null se o diretório é válido;
 	}
 }
-
